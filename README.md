71743d1f428373b55bf25d7a7761098621b5bafe
tejams <teja618@gmail.com>
2020-12-22 11:31:17 +0530

    First Program

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LeetCodes
-Solutions For LeetCode Problems
+Solutions For LeetCode Problems:
+
+1. Find First and Last Position of Element in Sorted Array
---
+++ src/Solution.java
@@ -0,0 +1,44 @@
+//Find First and Last Position of Element in Sorted Array
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] solutions=new int[2];
+        solutions[0]=calculateFirst(nums,target) ;
+        solutions[1]=calculateLast(nums,target);
+        return solutions;
+    }
+
+    public int calculateFirst(int[] nums,int target){
+        int low=0;
+        int high=nums.length-1;
+        int res=-1,mid;
+        while(low<=high){
+            mid=(low+high)/2;
+            if(nums[mid]>target){
+                high=mid-1;
+            }else if(nums[mid]<target){
+                low=mid+1;
+            }else {
+                res=mid;
+                high=mid-1;
+            }
+        }
+        return res;
+    }
+    public int calculateLast(int[] nums,int target){
+        int low=0;
+        int high=nums.length-1;
+        int res=-1,mid;
+        while(low<=high){
+            mid=(low+high)/2;
+            if(nums[mid]>target){
+                high=mid-1;
+            }else if(nums[mid]<target){
+                low=mid+1;
+            }else {
+                res=mid;
+                low=mid+1;
+            }
+        }
+        return res;
+    }
+}
